e8a2b74641a0ad827483b65d5a88c131032adcac
topoescobar <ivan90ge@gmail.com>
2023-08-03 13:25:46 -0300

    navbar format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { GlobalOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons'
-import { Button, Layout, Menu } from 'antd'
+import { Anchor, Button, Layout, Menu } from 'antd'
 import { Header } from 'antd/es/layout/layout'
 import React from 'react'
 
@@ -22,8 +22,9 @@ const HeaderComp = () => {
     <>
     <Layout>
       <Header style={{ display: 'flex', alignItems: 'center'}} >
-        <h1>My Tinerary</h1>
-
+      <h1>My Tinerary</h1>
+      
+      <div className='nav_menu'>
         <Menu
           theme="dark"
           mode="horizontal"
@@ -33,6 +34,7 @@ const HeaderComp = () => {
       <Button type="primary" icon={<UserOutlined />} >
             Login
       </Button>
+      </div>
 
       </Header>
 
