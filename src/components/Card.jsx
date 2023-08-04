224ea15d0aed24ecc3615f76a9d94918ab5299c2
topoescobar <ivan90ge@gmail.com>
2023-08-04 02:36:11 -0300

    card-hero

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,30 @@
-import { Button, Layout } from 'antd'
+import { Button, Col, Layout, Row } from 'antd'
 import React from 'react'
 import './card.css'
 
 const Card = () => {
+
+	const columnSpacing = { xs: 0, sm: 4, md: 8 };
+	const rowSpacing = { xs: 1, sm: 3, md: 6 };
+
 	return (
 		<Layout className='cardLayout'>
 			<div className='card'>
-				<h2>Find the perfect detination</h2>
-				<p>Our app will help you find the perfect path for your next trip.
-					With an easy-to-use interface and a host of itinerary options,
-					planning your next trip has never been easier.</p>
-				<Button type="primary" > View more </Button>
-				<div>
-					<img src='' />
-				</div>
-			</div>	
+				<Row gutter={[columnSpacing, rowSpacing]}>
+					<Col xs={24} sm={12} >
+						<h2>Find the perfect detination</h2>
+						<p>Our app will help you find the perfect path for your next trip.
+							With an easy-to-use interface and a host of itinerary options,
+							planning your next trip has never been easier.</p>
+						<Button type="primary" > View more </Button>
+					</Col>
+					<Col xs={24} sm={12}>
+						<div className='imgContainer'>
+							<img src="/mountains.png" alt="mountains" />
+						</div>
+					</Col>
+				</Row>
+			</div>
 		</Layout>
 	)
 }
