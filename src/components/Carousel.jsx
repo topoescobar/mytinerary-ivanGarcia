30ddce9301874b142ae3a87e1522ddf384225df7
topoescobar <ivan90ge@gmail.com>
2023-08-05 14:42:36 -0300

    carousel bullet buttons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,32 +1,32 @@
-import { Col, Collapse, Layout, Row } from 'antd'
+import { Button, Col, Collapse, Layout, Row } from 'antd'
 import React, { useState } from 'react'
 import './carousel.css'
 import { LeftCircleFilled, RightCircleFilled } from '@ant-design/icons'
 
 
 const places = [
-  { id: 0, imgUrl: '/forest.jpg', alt: 'forest' },
-  { id: 1, imgUrl: '/mountain.jpg', alt: 'mountain' },
-  { id: 2, imgUrl: '/lake.jpg', alt: 'lake' }
+  { id: 1, imgUrl: '/forest.jpg', alt: 'forest' },
+  { id: 2, imgUrl: '/mountain.jpg', alt: 'mountain' },
+  { id: 3, imgUrl: '/lake.jpg', alt: 'lake' }
 ]
 
 const CarouselComp = () => {
 
-  const [position, setPosition] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const prev = () => {
-    if (position > 0) {
-      setPosition(position - 1)
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1)
     } else {
-      setPosition(places.length - 1)
+      setCurrentIndex(places.length - 1)
     }
   }
 
   const next = () => {
-    if (position < places.length - 1) {
-      setPosition(position + 1)
+    if (currentIndex < places.length - 1) {
+      setCurrentIndex(currentIndex + 1)
     } else {
-      setPosition(0)
+      setCurrentIndex(0)
     }
   }
 
@@ -35,9 +35,8 @@ const CarouselComp = () => {
       <Row justify="center">
         <Col xs={24} sm={12}>
           <div className='carouselContent'>
-            {position}
             <LeftCircleFilled onClick={prev} />
-            <img src={places[position].imgUrl} alt={places[position].alt} />
+            <img src={places[currentIndex].imgUrl} alt={places[currentIndex].alt} />
             <RightCircleFilled onClick={next} />
           </div>
         </Col>
@@ -47,10 +46,23 @@ const CarouselComp = () => {
           <div className='bulletContainer'>
             {
               places.map((item) => {
-                if (item.id === position)
-                  return (<span>🟣</span>)
+                if ((item.id-1) === currentIndex)
+                  return (
+                    <Button
+                      type='primary'
+                      shape='circle'
+                      size='small'>
+                      {item.id}
+                    </Button>
+                  )
                 else return (
-                  <span onClick={() => setPosition(item.id)} >⚫</span>
+                  <Button
+                    type='dashed'
+                    shape='circle'
+                    size='small'
+                    onClick={() => setCurrentIndex(item.id-1)}>
+                    {item.id}
+                  </Button>
                 )
               })
             }
