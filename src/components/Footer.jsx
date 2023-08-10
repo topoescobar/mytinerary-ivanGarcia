623e9b447aef8e3181fe6f638f667226149d594d
topoescobar <ivan90ge@gmail.com>
2023-08-10 18:21:03 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import { Footer } from 'antd/es/layout/layout'
 import React from 'react'
 
-const FooterComp = () => {
+const FooterComp = ({ title, slogan, logo }) => {
   return (
     <>
-      <Footer> <p>footer</p> </Footer>
+      <Footer>
+        <span className='appName' > {title} </span> <p> {slogan} </p>
+      </Footer>
     </>
   )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,11 +4,19 @@ import FooterComp from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 
 const MainLayout = ({ children }) => {
+  const siteTitle= 'MyTinerary'
+  const slogan = 'Find your perfect trip'
+  const logo = '/travel-black.svg'
+
   return (
     <>
       <HeaderComp />
       <Outlet />
-      <FooterComp />
+      <FooterComp 
+      title= {siteTitle}
+      slogan= {slogan}
+      logo={logo}
+      />
     </>
   )
 }
