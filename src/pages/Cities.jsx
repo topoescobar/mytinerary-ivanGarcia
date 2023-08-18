9279a8ad64e2ac1ec74e2ea0e3bb8ec7f16d6f11
ivan-garcia <ivan90ge@gmail.com>
2023-08-18 19:05:35 -0300

    axios

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,8 +1,15 @@
 import { Layout } from 'antd'
-import React from 'react'
+import React, { useEffect } from 'react'
 import './cities.css'
 
 const Cities = () => {
+
+  const [places, setPlaces] = useEffect([])
+
+  useEffect(() => {
+     ('http://localhost:3001/api/events/')
+  })
+
   return (
     <Layout>
       <div className='containerFlex bgTransp citiesContainer'>
